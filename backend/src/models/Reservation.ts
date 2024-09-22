c82ef29ab97ad290d2eb21a8734c6b92b6e806e1
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2024-09-22 19:22:35 +1000

    fix(back): corregir y actualizar esquema de reserva en Mongoose
    - Se ha corregido la duplicación de los campos `numAdultos` y `numNinos` en el esquema de reservas.
    - Se ha establecido un valor predeterminado para `numAdultos` (1) y `numNinos` (0).
    - Se ha actualizado el campo `cancelada` con un valor predeterminado de `false` para indicar que las reservas no están canceladas por defecto.
    - Se ha añadido el campo `pasada` con un valor predeterminado de `false` para indicar si una reserva ya ha ocurrido.
    - El modelo `Reservation` ha sido corregido para evitar duplicaciones al exportarlo.

--- backend/src/models/Reservation.ts
+++ backend/src/models/Reservation.ts
@@ -1,14 +1,16 @@
 import mongoose, { Schema } from 'mongoose';
-import { IReservation } from '../interfaces/IReservation';
+import { IReservation } from '../../../shared/interfaces/IReservation';
 
 const ReservaSchema: Schema = new Schema<IReservation>({
   dia: { type: Date, required: true }, // Día de la reserva
   horario: { type: String, required: true }, // Horario de la reserva
   restaurante: { type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant', required: true }, // Referencia al restaurante
   cliente: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Referencia al cliente que hace la reserva
-  numAdultos: { type: Number, required: true }, // Número de adultos en la reserva
-  numNinos: { type: Number, required: true }, // Número de niños menores de 10 años en la reserva
+  numAdultos: { type: Number, required: true, default:1}, // Número de adultos en la reserva
+  numNinos: { type: Number, default:0 }, // Número de niños menores de 10 años en la reserva
+  cancelada: { type: Boolean, default: false }, // Estado de cancelación
+  pasada: {type:Boolean, default:false}
 });
 
-const Reservation = mongoose.model<IReservation>('Reservation', ReservaSchema, 'reservations'); // Crear el modelo de reserva basado en el esquema
+const Reservation = mongoose.model<IReservation>('Reservation', ReservaSchema, 'reservations');
 export default Reservation;
