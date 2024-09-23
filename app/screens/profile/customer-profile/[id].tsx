b46b4d56c587e53cc5546d37780e912770729912
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2024-09-23 12:13:27 +1000

    remove codigo innecesario

--- app/screens/profile/customer-profile/[id].tsx
+++ app/screens/profile/customer-profile/[id].tsx
@@ -7,13 +7,6 @@ import ProfileTabs from './profile-tabs';
 export default function CustomerProfile() {
   const { user, logout } = useAuth(); // Obtener el usuario autenticado y la función de logout del contexto
 
-  const handleSignOut = () => {
-    logout(); // Cerrar sesión del usuario
-  };
-
-  const handleChangePassword = () => {
-    // Lógica para cambiar la contraseña (aún no implementada)
-  };
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
