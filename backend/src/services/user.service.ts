34c4374f4962f258fd46a46b196741e89ef2e983
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2024-09-23 10:45:34 +1000

    refactor(back): renombrar y agregar servicios de usuario para manejo de contraseñas
    - Se han renombrado las funciones `findUserByEmail` y `registerUser` a `findUserByEmailService` y `registerUserService` para seguir una convención uniforme en los nombres de los servicios.
    - Se ha añadido el servicio `changePasswordService` para manejar el cambio de contraseñas, con validaciones para confirmar la nueva contraseña y verificar la contraseña actual.
    - El servicio comprueba si el usuario existe por su ID y si la contraseña antigua es correcta antes de proceder al cambio.

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -1,13 +1,14 @@
 import User from '../models/User';
 import crypto from 'crypto';
+import { comparePasswordService, hashPasswordService } from './auth.service';
 
-export const findUserByEmail = async (email: string) => {
+export const findUserByEmailService = async (email: string) => {
   return await User.findOne({ email }); // Buscar usuario por email en la base de datos
 };
 
-export const registerUser = async (nombre: string, apellido: string, email: string, hashedPassword: string) => {
+export const registerUserService = async (nombre: string, apellido: string, email: string, hashedPassword: string) => {
   // Verificar si el usuario ya existe en la base de datos
-  const existingUser = await findUserByEmail(email);
+  const existingUser = await findUserByEmailService(email);
   if (existingUser) {
     throw new Error('El correo ya está en uso.'); // Lanzar error si el email ya está registrado
   }
@@ -31,4 +32,38 @@ export const registerUser = async (nombre: string, apellido: string, email: stri
     console.error('Error al guardar el usuario:', error);
     throw error; // Lanzar error si ocurre algún problema al guardar
   }
+};
+
+
+export const changePasswordService = async (
+  userId: string,
+  oldPassword: string,
+  newPassword: string,
+  confirmNewPassword: string
+) => {
+  // Verificar si la nueva contraseña y su confirmación coinciden
+  if (newPassword !== confirmNewPassword) {
+    throw new Error('La nueva contraseña y la confirmación no coinciden');
+  }
+
+  // Buscar al usuario por su ID
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new Error('Usuario no encontrado');
+  }
+
+  // Verificar la contraseña actual
+  const isMatch = await comparePasswordService(oldPassword, user.password);
+  if (!isMatch) {
+    throw new Error('La contraseña actual es incorrecta');
+  }
+
+  // Hashear la nueva contraseña
+  const hashedNewPassword = await hashPasswordService(newPassword);
+
+  // Actualizar la contraseña en el registro del usuario
+  user.password = hashedNewPassword;
+  await user.save(); // Guardar cambios en la base de datos
+
+  return { message: 'Contraseña actualizada correctamente' };
 };
