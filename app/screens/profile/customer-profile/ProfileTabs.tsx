a1a933320779acccccbd0f861148cf52fe4647e9
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2024-09-24 11:53:01 +1000

    adopcion de nomenclatura PascalCase

--- app/screens/profile/customer-profile/ProfileTabs.tsx
+++ app/screens/profile/customer-profile/ProfileTabs.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-import AccountSettings from './account-settings';
-import ReservationsTab from './reservations-tab';
+import AccountSettings from './AccountSettings';
+import ReservationsTab from './ReservationsTab';
 
 const Tab = createMaterialTopTabNavigator();
 
