20bd238f09f54381a7a8ee6c86469c423d34cd4c
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2024-09-29 20:25:43 +1000

    fix(navigation): Actualización de rutas para pantallas de Login y Registro
    - Se corrigieron los enlaces de navegación para la pantalla de inicio de sesión, cambiando 'login' por 'Login'.
    - Se corrigieron los enlaces de navegación para la pantalla de registro, cambiando 'register' por 'Register'.

--- app/index.tsx
+++ app/index.tsx
@@ -6,10 +6,10 @@ export default function Index() {
   return (
     <View>
       {/* Enlace para navegar a la pantalla de login */}
-      <Link href="../screens/auth/login">Login</Link>
+      <Link href="../screens/auth/login/Login">Inicia sesión</Link>
       
       {/* Enlace para navegar a la pantalla de registro */}
-      <Link href="../screens/auth/register">Register</Link>
+      <Link href="../screens/auth/register/Register">Regístrate</Link>
       
       {/* Texto que indica la pantalla actual */}
       <Text>Home screen</Text>
