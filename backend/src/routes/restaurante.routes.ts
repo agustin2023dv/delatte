f85e3116c937015e5788ab1e6bb89b30a2101087
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2024-09-29 20:06:05 +1000

    refactor(routes): eliminada validación de manager y simplificación de rutas de restaurantes
    - Comentada la middleware `managerOfRestaurantMiddleware` en la ruta de actualización de restaurante.
    - Ajustadas las importaciones para eliminar referencias duplicadas o no necesarias.
    - Conservada la validación de creación de restaurante utilizando `validateRestaurantCreation`.
    - Se sigue validando la actualización del restaurante con `validateRestaurantUpdate`.

--- backend/src/routes/restaurante.routes.ts
+++ backend/src/routes/restaurante.routes.ts
@@ -1,19 +1,17 @@
 import { Router } from 'express';
-import { registerRestaurantAndManagerController, getRestaurantByIdController, updateRestaurantController, getRestaurantsByManagerIdController } 
+import { registerRestaurantAndManagerController, getRestaurantByIdController, 
+  updateRestaurantController, getRestaurantsByManagerIdController } 
 from '../controllers/restaurante.controller';
 import { authMiddleware } from '../middlewares/auth.middleware';
 import { roleMiddleware } from '../middlewares/role.middleware';
-import { managerOfRestaurantMiddleware } from '../middlewares/restaurant.middleware';
-import { validateRestaurantCreation, validateRestaurantUpdate } from 'shared/utils/restaurant.validation';
+//import { managerOfRestaurantMiddleware } from '../middlewares/restaurant.middleware';
+import {  validateRestaurantUpdate } from '../../../shared/utils/restaurant.validation';
 
 const router = Router();
 
 //* Ruta para CREAR un restaurante (solo superadmins o managers pueden crear)
 router.post(
   '/register-restaurant', 
-  authMiddleware, 
-  roleMiddleware(['superadmin', 'manager']),
-  validateRestaurantCreation,  // Validar solo nombre y dirección en la creación
   registerRestaurantAndManagerController
 );
 
@@ -28,7 +26,7 @@ router.get(
 router.put(
   '/:id', 
   authMiddleware, 
-  managerOfRestaurantMiddleware, // Verificar que sea el manager del restaurante o superadmin
+  //managerOfRestaurantMiddleware, // Verificar que sea el manager del restaurante o superadmin
   validateRestaurantUpdate,  // Validar todos los campos posibles en la actualización
   updateRestaurantController
 );
