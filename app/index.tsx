adaafddb6a08162b039e2ec4070bc2b46eb80887
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2024-09-30 08:38:42 +1000

    feat(home): Mostrar opciones basadas en el estado de autenticación del usuario
    - Se agregó una verificación del estado de autenticación del usuario (`isAuthenticated`) en el componente `Home`.
    - Si el usuario está autenticado, se muestra un mensaje y un botón para cerrar sesión.
    - Si el usuario no está autenticado, se muestran los enlaces para iniciar sesión y registrarse.

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,37 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 import { Link } from "expo-router";
+import { useAuth } from "./contexts/AuthContext";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 // Componente principal que muestra enlaces de navegación
 export default function Home() {
-  return (
-    <View>
-      {/* Enlace para navegar a la pantalla de login */}
-      <Link href="../screens/auth/login/Login">Inicia sesión</Link>
-      
-      {/* Enlace para navegar a la pantalla de registro */}
-      <Link href="../screens/auth/register/Register">Regístrate</Link>
-      
-      {/* Texto que indica la pantalla actual */}
-      <Text>Home screen</Text>
-    </View>
-  );
-}
+  const user = useAuth();
+
+  
+  if(user.isAuthenticated){
+    return(<>
+      <SafeAreaView>
+        <Text>ok</Text>
+        <Button title="Log out"/>
+      </SafeAreaView>
+    
+    </>)
+
+  }
+  else{
+    return (
+      <View>
+        {/* Enlace para navegar a la pantalla de login */}
+        <Link href="../screens/auth/login/Login">Inicia sesión</Link>
+        
+        {/* Enlace para navegar a la pantalla de registro */}
+        <Link href="../screens/auth/register/Register">Regístrate</Link>
+        
+        {/* Texto que indica la pantalla actual */}
+        <Text>Home screen</Text>
+      </View>
+    );
+  }
+  }
+  
+  
