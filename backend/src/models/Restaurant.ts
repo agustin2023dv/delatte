6f794395de9004508f48f2fdfc8250df042f085f
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2024-09-29 20:07:10 +1000

    refactor(schema): ajustada la obligatoriedad de campos en RestaurantSchema
    - Se ha eliminado la restricción `required` en los campos `nombre`, `direccion`, `localidad`, `telefonos`, `emailContacto`, `horarios` y `capacidadMesas`.
    - Se ha cambiado la estructura del campo `managers` añadiendo un valor por defecto de `[]`.
    - Se han mantenido el campo `ultimaActualizacion` con un valor por defecto `Date.now` y `estaAbierto` con un valor por defecto `true`.

--- backend/src/models/Restaurant.ts
+++ backend/src/models/Restaurant.ts
@@ -2,26 +2,26 @@ import mongoose, { Schema } from 'mongoose';
 import { IRestaurant } from '../../../shared/interfaces/IRestaurant';
 
 const RestaurantSchema = new Schema<IRestaurant>({
-  nombre: { type: String, required: true }, // Nombre del restaurante
-  direccion: { type: String, required: true }, // Dirección del restaurante
-  localidad: { type: String, required: true }, // Localidad o ciudad del restaurante
-  telefonos: [{ type: String, required: true }], // Lista de teléfonos de contacto del restaurante
-  emailContacto: [{ type: String, required: true }], // Correo electrónico de contacto
+  nombre: { type: String }, // Nombre del restaurante
+  direccion: { type: String }, // Dirección del restaurante
+  localidad: { type: String }, // Localidad o ciudad del restaurante
+  telefonos: [{ type: String }], // Lista de teléfonos de contacto del restaurante
+  emailContacto: { type: String }, // Correo electrónico de contacto
   logo: { type: String }, // URL de la imagen del logo
   galeriaFotos: [{ type: String }], // Array de URLs de la galería de fotos
   horarios: [{ // Lista de horarios de apertura y cierre por día
-    dia: { type: String, required: true },
-    horaApertura: { type: String, required: true },
-    horaCierre: { type: String, required: true }
+    dia: { type: String},
+    horaApertura: { type: String },
+    horaCierre: { type: String }
   }],
   capacidadMesas: [{ // Capacidad del restaurante, definido por mesas y personas por mesa
-    cantidad: { type: Number, required: true },
-    personasPorMesa: { type: Number, required: true }
+    cantidad: { type: Number },
+    personasPorMesa: { type: Number }
   }],
   menuComida: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Menu' }],  // Relación con el modelo de menú de comidas
   menuBebidas: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Menu' }], // Relación con el modelo de menú de bebidas
   menuPostres: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Menu' }],  // Relación con el modelo de menú de postres
-  managers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Relación con el modelo de User para los managers
+  managers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User',default:[] }], // Relación con el modelo de User para los managers
   estaAbierto: { type: Boolean, default: true }, // Indica si el restaurante está operando o no
   ultimaActualizacion: { type: Date, default: Date.now } // Fecha de la última actualización de datos
 });
