da2df03500c611fdb10a2cd9cc965656e9e20825
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2024-09-29 20:04:17 +1000

    refactor(back): hacer opcionales ciertos campos de IRestaurant y añadir IRestaurantCreate
    - Ajustados varios campos en la interfaz `IRestaurant` como opcionales para mayor flexibilidad en el manejo de datos.
    - Añadida la nueva interfaz `IRestaurantCreate` para la creación de restaurantes con los campos básicos: `nombre`, `dirección` y `emailContacto`.
    - Estos cambios facilitan la creación y actualización de restaurantes sin requerir todos los campos en cada operación.

--- shared/interfaces/IRestaurant.ts
+++ shared/interfaces/IRestaurant.ts
@@ -1,27 +1,33 @@
 import { Document, ObjectId } from 'mongoose';
 
 export interface IRestaurant extends Document {
-  _id: ObjectId | string; // ID del restaurante
+  _id?: ObjectId | string; // ID del restaurante
   nombre: string; // Nombre del restaurante
   direccion: string; // Dirección del restaurante
-  localidad: string; // Localidad donde se encuentra el restaurante
-  telefonos: string[];  // Lista de teléfonos de contacto
-  emailContacto: string[]; // Email de contacto del restaurante
+  localidad?: string; // Localidad donde se encuentra el restaurante
+  telefonos?: string[];  // Lista de teléfonos de contacto
+  emailContacto: string; // Email de contacto del restaurante
   logo?: string; // URL de la imagen del logo
   galeriaFotos?: string[]; // URLs de las imágenes de la galería
-  horarios: { // Lista de horarios de apertura y cierre por día
-    dia: string;
-    horaApertura: string;
-    horaCierre: string;
+  horarios?: { // Lista de horarios de apertura y cierre por día
+    dia?: string;
+    horaApertura?: string;
+    horaCierre?: string;
   }[];
-  capacidadMesas: { // Capacidad del restaurante en términos de mesas y personas por mesa
-    cantidad: number;
-    personasPorMesa: number;
+  capacidadMesas?: { // Capacidad del restaurante en términos de mesas y personas por mesa
+    cantidad?: number;
+    personasPorMesa?: number;
   }[];
-  menuComida: ObjectId[];  // IDs de los menús de comida relacionados
-  menuBebidas: ObjectId[];  // IDs de los menús de bebidas relacionados
-  menuPostres: ObjectId[];  // IDs de los menús de postres relacionados
-  managers: ObjectId[]; // Managers del restaurante. Usuarios con rol {manager}
-  estaAbierto: boolean;
-  ultimaActualizacion: Date;
+  menuComida?: ObjectId[];  // IDs de los menús de comida relacionados
+  menuBebidas?: ObjectId[];  // IDs de los menús de bebidas relacionados
+  menuPostres?: ObjectId[];  // IDs de los menús de postres relacionados
+  managers?: ObjectId[]; // Managers del restaurante. Usuarios con rol {manager}
+  estaAbierto?: boolean;
+  ultimaActualizacion?: Date;
+}
+
+export interface IRestaurantCreate {
+  nombre: string;
+  direccion: string;
+  emailContacto: string;
 }
