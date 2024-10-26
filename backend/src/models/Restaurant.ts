1f6f21623b321b9a4c9fea583c3138ac8eb0de50
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2024-10-26 16:13:58 +1100

    fix(restaurant-schema): establecer valores por defecto en arrays y calificación
    - Agregados valores por defecto para arrays (`telefonos`, `galeriaFotos`, `menuComida`, `menuBebidas`, `menuPostres`, `managers`) en el esquema de `Restaurant`.
    - Ajustada la propiedad `calificacion` con un valor por defecto de 1 y rango de 1 a 5.
    - Estos cambios aseguran consistencia en la creación de nuevos documentos de restaurantes y previenen errores en arrays vacíos.

--- backend/src/models/Restaurant.ts
+++ backend/src/models/Restaurant.ts
@@ -5,10 +5,11 @@ const RestaurantSchema = new Schema<IRestaurant>({
   nombre: { type: String }, // Nombre del restaurante
   direccion: { type: String }, // Dirección del restaurante
   localidad: { type: String, default: 'Montevideo' }, // Localidad o ciudad del restaurante
-  telefonos: [{ type: String }], // Lista de teléfonos de contacto del restaurante
+  telefonos: [{ type: String, default: [] }], // Lista de teléfonos de contacto del restaurante
   emailContacto: { type: String }, // Correo electrónico de contacto
   logo: { type: String , default:'Logo restaurante' }, // URL de la imagen del logo
-  galeriaFotos: [{ type: String }], // Array de URLs de la galería de fotos
+  galeriaFotos: [{ type: String , default: []}], // Array de URLs de la galería de fotos
+  calificacion: {type: Number, default:1, min:1, max:5},
   horarios: [{ // Lista de horarios de apertura y cierre por día
     dia: { type: String},
     horaApertura: { type: String },
@@ -18,9 +19,9 @@ const RestaurantSchema = new Schema<IRestaurant>({
     cantidad: { type: Number, default:1 },
     personasPorMesa: { type: Number, default:2 }
   }],
-  menuComida: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Menu' }],  // Relación con el modelo de menú de comidas
-  menuBebidas: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Menu' }], // Relación con el modelo de menú de bebidas
-  menuPostres: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Menu' }],  // Relación con el modelo de menú de postres
+  menuComida: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Menu', default: [] }],  // Relación con el modelo de menú de comidas
+  menuBebidas: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Menu', default: [] }], // Relación con el modelo de menú de bebidas
+  menuPostres: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Menu', default: [] }],  // Relación con el modelo de menú de postres
   managers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User',default:[] }], // Relación con el modelo de User para los managers
   estaAbierto: { type: Boolean, default: true }, // Indica si el restaurante está operando o no
   ultimaActualizacion: { type: Date, default: Date.now } // Fecha de la última actualización de datos
