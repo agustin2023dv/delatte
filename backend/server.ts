42fa4a12d7093aa511772efe9376a59a14685517
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2024-10-26 16:09:12 +1100

    feat(server): agregar rutas para reseñas en el servidor
    - Añadida la ruta `/api/reviews` en el servidor para gestionar las reseñas (reviews).
    - Incluye `reviewRoutes` en `index.js`, permitiendo la creación, actualización y eliminación de reseñas asociadas a restaurantes.
    - Mejora la estructura del API y facilita la administración de reseñas en el backend.

--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import usuarioRoutes from './src/routes/usuario.routes';
 import restaurantRoutes from './src/routes/restaurante.routes';
 import reservationRoutes from './src/routes/reserva.routes';
+import reviewRoutes from './src/routes/resena.routes';
 import { connectDB } from './db';
 import cors from 'cors';
 
@@ -27,6 +28,8 @@ app.use('/api/users', usuarioRoutes); // Usar las rutas definidas para usuarios 
 app.use('/api/restaurantes', restaurantRoutes); // Usar las rutas definidas para usuarios bajo el endpoint /api/restaurantes
 
 app.use('/api/reservas', reservationRoutes); // Usar las rutas definidas para usuarios bajo el endpoint /api/users
+
+app.use('/api/reviews',reviewRoutes); 
 
 app.listen(port, () => {
   // Confirmar que el servidor está corriendo y en qué puerto
