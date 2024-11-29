74b370deab7f7bb052eed502d939142d326e2681
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2024-11-29 13:58:17 +1100

    refactor(restaurant-list): actualizar la lista de restaurantes para aceptar props externas y manejar loading
    - Refactorización del componente `RestaurantList` para aceptar `restaurants` y `loading` como props externas.
    - Eliminación del manejo interno de estado y fetching, delegando la responsabilidad de los datos al componente padre.
    - Ajuste de la cantidad de columnas en el diseño (`numColumns` ajustado a 4).
    - Simplificación del componente para mayor reusabilidad y modularidad.
    - Mantenimiento de la funcionalidad existente para renderizar tarjetas de restaurantes con estilos básicos.

--- components/restaurant/RestaurantList.tsx
+++ components/restaurant/RestaurantList.tsx
@@ -1,51 +1,26 @@
-import React, { useEffect, useState } from "react";
-import {  Text, StyleSheet, FlatList, View } from "react-native";
-import { getAllRestaurantsService } from "@/app/services/restaurant.service";
+import React from "react";
+import { Text, StyleSheet, FlatList, View } from "react-native";
 import { IRestaurant } from "shared/interfaces/IRestaurant";
 import { RestaurantCard } from "components/restaurant/RestaurantCard";
 
-export function RestaurantList() {
-    const [restaurants, setRestaurants] = useState<Partial<IRestaurant>[]>([]);
-    const [loading, setLoading] = useState(true);
- 
-    
-    useEffect(() => {
-        async function fetchRestaurants() {
-            try {
-                const data = await getAllRestaurantsService();
-                console.log("Datos de restaurantes:", data);
-                setRestaurants(data); 
-            } catch (error) {
-                console.error("Error al obtener XX restaurantes:", error);
-            } finally {
-                setLoading(false);
-            }
-        }
+interface RestaurantListProps {
+    restaurants: Partial<IRestaurant>[];
+    loading?: boolean;
+}
 
-        fetchRestaurants();
-    }, []);
-
+export function RestaurantList({ restaurants, loading = false }: RestaurantListProps) {
     if (loading) {
         return <Text style={styles.loadingText}>Cargando restaurantes...</Text>;
     }
 
-
     return (
-        <>
-
         <FlatList
             data={restaurants}
             renderItem={({ item }) => <RestaurantCard {...item} />}
             keyExtractor={(item) => item._id?.toString() || ""}
-            numColumns={5}
+            numColumns={4} 
             contentContainerStyle={styles.container}
         />
-
-
-
-
-        </>
-            
     );
 }
 
@@ -58,5 +33,3 @@ const styles = StyleSheet.create({
         marginTop: 20,
     },
 });
-
-
