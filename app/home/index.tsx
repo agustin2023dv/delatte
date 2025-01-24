996e50587d04e0e1542a362094b16fb940b9e5a8
lualzugaray <lucia.alzugaray24@gmail.com>
2025-01-23 22:37:27 -0300

    feat: agregar Navbar con botones de navegación y cierre de sesión

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,17 +1,21 @@
 import React from "react";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, StyleSheet, View, Image } from "react-native";
 import SearchBar from "components/search/SearchBar";
 import LogOutButton from "components/buttons/LogOutButton";
+import NavBar from "../components/navbar/navbar";
 
 export default function HomeScreen() {
-
-
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-        <LogOutButton />
-        <SearchBar/>
-        
-
+    <SafeAreaView style={styles.safeContainer}>
+      <NavBar />
+      <SearchBar />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  safeContainer: {
+    flex: 1,
+    backgroundColor: "#F7EBE1",
+  },
+});
